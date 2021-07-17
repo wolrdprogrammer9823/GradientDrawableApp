3d901ef9c500eaf184dd92ca3f51843fffdface0
sharker <bigworld51931@126.com>
2021-07-17 16:16:26 +0800

    1.使用协程实现防止快速点击功能

--- app/src/main/java/com/wolfsea/gradientdrawableapp/MainActivity.kt
+++ app/src/main/java/com/wolfsea/gradientdrawableapp/MainActivity.kt
@@ -2,13 +2,44 @@ package com.wolfsea.gradientdrawableapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.SystemClock
+import android.util.Log
+import android.view.View
+import com.wolfsea.gradientdrawableapp.uitl.logMessage
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.actor
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), CoroutineScope by MainScope() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        linear_layout.onClickStart {
+            logMessage("onClickStart:${System.currentTimeMillis()}")
+            delay(3000)
+        }
     }
+
+    @ObsoleteCoroutinesApi
+    fun View.onClickStart(action: suspend () -> Unit) {
+        val eventActor = actor<Unit>(Dispatchers.Main) {
+            for (event in channel) {
+                action()
+            }
+        }
+
+        setOnClickListener {
+            eventActor.offer(Unit)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        //取消协程,子协程也随之取消
+        cancel()
+    }
+
 }
---
+++ app/src/main/java/com/wolfsea/gradientdrawableapp/uitl/LogUtil.kt
@@ -0,0 +1,15 @@
+package com.wolfsea.gradientdrawableapp.uitl
+import android.util.Log
+
+/**
+ *@desc  打印日志工具
+ *@author liuliheng
+ *@time 2021/7/17  16:01
+ **/
+
+const val TAG = "drawable"
+
+fun logMessage(message: String) {
+
+    Log.d(TAG, message)
+}
